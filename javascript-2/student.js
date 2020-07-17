76b6bd10402b6faf40668f32d157a54b53bb02a6
vhamilton08 <vhamilton08@gmail.com>
2020-07-17 14:14:13 -0500

    skillscheck

--- javascript-2/student.js
+++ javascript-2/student.js
@@ -5,26 +5,32 @@
 //Create a variable called 'lovesCode' and assign it a value of true or false (as a boolean) depending on if you love code or not. (It's okay if you don't yet!)
 
 // Code Here
-
+lovesCode = true
 //////////////////STEP 2////////////////////
 //Create a variable called 'age' that has a value equal to your age as a number. 
 
 // Code Here
-
+const age = 23
 //////////////////STEP 3////////////////////
 //Create a variable called 'name' and assign it a value of your name as a string.  
 
 // Code Here
-
+const name = "vincent"
 //////////////////STEP 4////////////////////
 //Create a variable called 'phrase' that is initialized as an empty string. 
 
 // Code Here
-
+let phrase = ""
 //////////////////STEP 5////////////////////
 //Create an if statement that checks to see if the lovesCode variable is equal to true. If it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
 
 // Code Here
-
+function lovesCode(){
+if(lovesCode === true){
+    return phrase = "I love code"
+} else {
+    return phrase = "I do not love to code"
+}
+}
 //////////////////Wrap Up////////////////////
 // Refresh the index.html page in your browser. You should now see a poem that uses the variables from above.
